d875ab5cdec4d9c252d7fc5761a59f4e612a3cb4
Minjun Ko <kohmj0324@naver.com>
2023-09-04 21:14:38 +0900

    001

--- Step/001.py
+++ Step/001.py
@@ -1,2 +1,74 @@
-# 1
+#1
 print("Hello World!")
+
+
+#2
+A, B = map(int, input().split())
+print(A + B)
+
+
+#3
+A, B = map(int, input().split())
+print(A - B)
+
+
+#4
+A, B = map(int, input().split())
+print(A * B)
+
+
+#5
+A, B = map(int, input().split())
+print(A / B)
+
+
+#6
+A, B = map(int, input().split())
+print(A + B)
+print(A - B)
+print(A * B)
+print(A // B)
+print(A % B)
+
+
+#7
+print(input()+"??!")
+
+
+#8
+print(int(input()) - 543)
+
+
+#9
+A, B, C = map(int, input().split())
+print((A+B)%C)
+print(((A%C)+(B%C))%C)
+print((A*B)%C)
+print(((A%C)*(B%C))%C)
+
+#10
+A = int(input())
+B = int(input())
+print(A*(B%10))
+print(A*((B//10)%10))
+print(A*(B//100))
+print(A*B)
+
+
+#11
+A, B, C = map(int, input().split())
+print(A+B+C)
+
+
+#12
+print("\    /\\")
+print(" )  ( ')")
+print("(  /  )")
+print(" \(__)|")
+
+#13
+print("|\_/|")
+print("|q p|   /}")
+print("( 0 )\"\"\"\\")
+print("|\"^\"`    |")
+print("||_/=\\\\__|")
