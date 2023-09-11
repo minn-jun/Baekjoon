3a09725007213d753e93bffdcaa24041f9512a0a
Minjun Ko <kohmj0324@naver.com>
2023-09-11 18:03:35 +0900

    011

---
+++ Step/011.py
@@ -0,0 +1,37 @@
+# #24262
+# n = int(input())
+# print(1)  # 수행횟수
+# print(0)  # 수행횟수를 다항식으로 나타냈을 때 최고차항의 차수
+
+
+# #24263
+# n = int(input())
+# print(n)
+# print(1)
+
+
+# #24264
+# n = int(input())
+# print(n**2)
+# print(2)
+
+
+# #24265
+# n = int(input())
+# print(int(n*(n-1)/2))
+# print(2)
+
+
+# #24266
+# n = int(input())
+# print(n**3)
+# print(3)
+
+
+# #24267
+# n = int(input())
+# print(int(n*(n-1)*(n-2)/6))
+# print(3)
+
+
+#24313
